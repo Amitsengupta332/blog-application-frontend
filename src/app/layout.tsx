d3d04413467684780b056bbbf28e69b1dc9def99
amitsengupta <amitsengupta332@gmail.com>
2026-01-15 00:21:39 +0600

    Implementing multiple layout using Route Groups

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/layout/Navbar";
+
 import { ThemeProvider } from "@/providers/ThemeProvider";
 
 const geistSans = Geist({
@@ -34,7 +34,6 @@ export default function RootLayout({
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange>
-          <Navbar />
           {children}
         </ThemeProvider>
       </body>
