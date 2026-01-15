365c371df2be27111648fc07dac3e4e23d57ac8f
amitsengupta <amitsengupta332@gmail.com>
2026-01-15 23:11:31 +0600

    Recap of parallel routes and implementing role based sidebar

--- src/app/(dashboardLayout)/@admin/default.tsx
+++ src/app/(dashboardLayout)/@admin/default.tsx
@@ -1,7 +1,3 @@
- 
-
 export default function AdminDefault() {
-  return (
-    <div>AdminDefault</div>
-  )
+  return null;
 }
--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -13,46 +13,67 @@ import {
   SidebarMenuItem,
   SidebarRail,
 } from "@/components/ui/sidebar"
+import { adminRoutes } from "@/routes/adminRoutes";
+import { userRoutes } from "@/routes/userRoutes";
+import { Route } from "@/types";
+import Link from "next/link";
  
 
 // This is sample data.
-const data = {
-  versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
-  navMain: [
-    {
-      title: "Getting Started",
-      url: "#",
-      items: [
-        {
-          title: "Write Blog",
-          url: "/dashboard/write-blog",
-        },
-        {
-          title: "Analytics",
-          url: "/dashboard/analytics",
-        },
-      ],
-    },
+// const data = {
+//   versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
+//   navMain: [
+//     {
+//       title: "Getting Started",
+//       url: "#",
+//       items: [
+//         {
+//           title: "Write Blog",
+//           url: "/dashboard/write-blog",
+//         },
+//         {
+//           title: "Analytics",
+//           url: "/dashboard/analytics",
+//         },
+//       ],
+//     },
    
  
-  ],
-}
+//   ],
+// }
 
-export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+export function AppSidebar({
+  user,
+  ...props
+}: {
+  user: { role: string } & React.ComponentProps<typeof Sidebar>;
+}) {
+  let routes: Route[] = [];
+
+  switch (user.role) {
+    case "admin":
+      routes = adminRoutes;
+      break;
+    case "user":
+      routes = userRoutes;
+      break;
+    default:
+      routes = [];
+      break;
+  }
+
   return (
     <Sidebar {...props}>
- 
       <SidebarContent>
-       
-        {data.navMain.map((item) => (
+        {routes.map((item) => (
           <SidebarGroup key={item.title}>
             <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
             <SidebarGroupContent>
               <SidebarMenu>
                 {item.items.map((item) => (
                   <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild isActive={item.isActive}>
-                      <a href={item.url}>{item.title}</a>
+                    <SidebarMenuButton asChild>
+                      <Link href={item.url}>{item.title}</Link>
                     </SidebarMenuButton>
                   </SidebarMenuItem>
                 ))}
@@ -63,5 +84,5 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarContent>
       <SidebarRail />
     </Sidebar>
-  )
+  );
 }
