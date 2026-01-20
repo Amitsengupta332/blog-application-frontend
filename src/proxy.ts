d3c6566423cacb1df19e4d42af86d99ceca76109
amitsengupta <amitsengupta332@gmail.com>
2026-01-20 22:30:36 +0600

    Private routes using proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,10 +1,43 @@
 import { NextRequest, NextResponse } from "next/server";
+import { userService } from "./service/user.service";
+import { Roles } from "./constants/roles";
 
 export async function proxy(request: NextRequest) {
-    // const pathName = request.nextUrl.pathname;
-    console.log(request.url)
+  // const pathName = request.nextUrl.pathname;
+  // console.log(request.url)
 
-    return NextResponse.next();
+  const pathname = request.nextUrl.pathname;
+
+  let isAuthenticated = false;
+  let isAdmin = false;
+
+  const { data } = await userService.getSession();
+
+  if (data) {
+    isAuthenticated = true;
+    isAdmin = data.user.role === Roles.admin;
+  }
+
+  //* User in not authenticated at all
+  if (!isAuthenticated) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  
+  //* User is authenticated and role = ADMIN
+  //* User can not visit user dashboard
+    if (isAdmin && pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+  }
+
+  //* User is authenticated and role = USER
+  //* User can not visit admin-dashboard
+  if (!isAdmin && pathname.startsWith("/admin-dashboard")) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+
+  return NextResponse.next();
 }
 
 export const config = {
