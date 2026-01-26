9bddf4b4c7cbe3fb5e986c175dab4bbc3768fde1
amitsengupta <amitsengupta332@gmail.com>
2026-01-26 20:01:59 +0600

    Quick recap on post flow and fixing role issue

---
+++ src/app/(dashboardLayout)/@user/dashboard/create-blog/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateBlogPage = () => {
+  return <div>CreateBlogPage is here</div>;
+};
+
+export default CreateBlogPage;
--- src/app/(dashboardLayout)/@user/dashboard/page.tsx
+++ src/app/(dashboardLayout)/@user/dashboard/page.tsx
@@ -1,7 +1,7 @@
+import { redirect } from "next/navigation";
+
  
 
 export default function UserDashboard() {
-  return (
-    <div>UserDashboard</div>
-  )
+  return redirect("/dashboard/create-blog");
 }
